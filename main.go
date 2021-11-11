ad3fabb910f51bd87a6a205d6962c3ea821aeab6
Zeebrow <mzborowski@yahoo.com>
2021-11-10 21:09:12 -0500

    remove println()'s and leave evidence when dying

--- main.go
+++ main.go
@@ -30,18 +30,14 @@ func main() {
 	var modeFlag = flag.Bool("l", false, "light mode (dark mode is default)")
 	var fromFile = flag.String("f", "", "from file filename")
 	flag.Parse()
-	fmt.Printf("%v %v\n", *modeFlag, *fromFile)
 
 	// https://stackoverflow.com/a/43947435/14494128
 	fd1, err := os.Stdin.Stat()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal("Something went wrong!\n%s\n", err)
 	}
-	fmt.Println("start")
 	if fd1.Mode()&os.ModeCharDevice == 0 {
-
-		fmt.Println("data is from a file")
-		fmt.Println("piped")
+		// from pipe
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
 			mdtext = append(mdtext, scanner.Text()+"\n")
@@ -51,11 +47,11 @@ func main() {
 		}
 
 	} else {
-		fmt.Println("from file?")
+		// from file
 		fmt.Println(*fromFile)
 		data, err := os.Open(*fromFile)
 		if err != nil {
-			fmt.Println(err)
+			log.Fatal("Something went wrong!\n%s\n", err)
 		}
 		scanner := bufio.NewScanner(data)
 		for scanner.Scan() {
