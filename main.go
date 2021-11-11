bac181b727e05d9f44ad770d0d4115a5c047a1c4
Zeebrow <mzborowski@yahoo.com>
2021-11-10 22:58:34 -0500

    error messages and exit statuses

--- main.go
+++ main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 
 	"github.com/charmbracelet/glamour"
 )
 
+var ERR_WTF = 1
+var ERR_NOF = 2
+var ERR_RTFM = 3
+
 var mdtext []string
+var progname = "fmt-md-text"
 
 func pprint(md string, lightMode *bool) {
 	var mode = "dark"
@@ -20,9 +25,13 @@ func pprint(md string, lightMode *bool) {
 	}
 	out, err := glamour.Render(md, mode)
 	if err != nil {
-		log.Fatal(err.Error())
+		fmt.Printf(err.Error())
 	}
 	fmt.Print(out)
+}
+
+func usage() {
+	fmt.Printf("usage: %s [-l] [-f FILE.md]\n", progname)
 }
 
 func main() {
@@ -34,31 +43,45 @@ func main() {
 	// https://stackoverflow.com/a/43947435/14494128
 	fd1, err := os.Stdin.Stat()
 	if err != nil {
-		log.Fatal("Something went wrong!\n%s\n", err)
+		fmt.Printf("Something went wrong!\n%s\n", err)
+		os.Exit(ERR_WTF)
 	}
 	if fd1.Mode()&os.ModeCharDevice == 0 {
 		// from pipe
+		fmt.Printf("pipe\n")
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
 			mdtext = append(mdtext, scanner.Text()+"\n")
 		}
 		if scanner.Err() != nil {
-			log.Fatal("Something went wrong!\n%s\n", scanner.Err())
+			fmt.Printf("Something went wrong!\n%s\n", scanner.Err())
+			os.Exit(ERR_WTF)
 		}
 
 	} else {
 		// from file
-		fmt.Println(*fromFile)
+		if *fromFile == "" {
+			fmt.Printf("No file provided! %s\n", *fromFile)
+			usage()
+			os.Exit(ERR_RTFM)
+		}
+		if _, err := os.Stat(*fromFile); errors.Is(err, os.ErrNotExist) {
+			fmt.Printf("No such file: %s\n", *fromFile)
+			os.Exit(ERR_NOF)
+		}
+
 		data, err := os.Open(*fromFile)
 		if err != nil {
-			log.Fatal("Something went wrong!\n%s\n", err)
+			fmt.Printf("Something went wrong!\n%s\n", err)
+			os.Exit(3)
 		}
 		scanner := bufio.NewScanner(data)
 		for scanner.Scan() {
 			mdtext = append(mdtext, scanner.Text()+"\n")
 		}
 		if scanner.Err() != nil {
-			log.Fatal("Something went wrong!\n%s\n", scanner.Err())
+			fmt.Printf("Something went wrong!\n%s\n", scanner.Err())
+			os.Exit(ERR_WTF)
 		}
 
 	}
